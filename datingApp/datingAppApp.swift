dd57379f8e0cfdfeb30cc40de11ba9f6bf3cdbb4
Kayode <kayodefredrickbalogun@gmail.com>
2024-07-18 01:59:17 +0100

    Initial Commit

---
+++ datingApp/datingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  datingAppApp.swift
+//  datingApp
+//
+//  Created by Balogun Kayode on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct datingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
