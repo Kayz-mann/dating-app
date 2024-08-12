a4710e4009a7324bbf1e6578bdc194c754c15866
Kayode <kayodefredrickbalogun@gmail.com>
2024-08-12 10:21:32 +0100

    fix: package dependency intallation for alamofire

--- Pods/Target Support Files/Pods-datingApp-datingAppUITests/Pods-datingApp-datingAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_datingApp_datingAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_datingApp_datingAppUITests
-@end
--- datingApp/Core/Auth/Service/CloudinaryService.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CloudinaryService.swift
-//  datingApp
-//
-//  Created by Balogun Kayode on 10/08/2024.
-//
-
-//import Alamofire
